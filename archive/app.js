b32115a07492410fee6c3734ae67432e07bbc452
henriqueneubert <neubert.study@gmail.com>
2022-10-08 12:05:39 -0300

    feat: front-end started

---
+++ archive/app.js
@@ -0,0 +1,19 @@
+const nine = document.querySelector('#sevn')
+const eight = document.querySelector('#sevn')
+const seven = document.querySelector('#sevn')
+const six = document.querySelector('#sevn')
+const five = document.querySelector('#sevn')
+const four = document.querySelector('#sevn')
+const three = document.querySelector('#sevn')
+const two = document.querySelector('#sevn')
+const one = document.querySelector('#sevn')
+const zero = document.querySelector('#sevn')
+
+
+const dot = document.querySelector('#sevn')
+const minus = document.querySelector('#sevn')
+const equal = document.querySelector('#sevn')
+const more = document.querySelector('#sevn')
+
+const cheese = document.querySelector('#sevn')
+const Divided = document.querySelector('#sevn')
